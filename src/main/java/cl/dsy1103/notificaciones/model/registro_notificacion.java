3c6937c903cbb4fd9d052103b4b9144e3c9959fe
Matias Tiznado <tiznado@duocuc.cl>
2026-05-15 02:59:44 -0400

    model listo notificaciones v1

---
+++ src/main/java/cl/dsy1103/notificaciones/model/registro_notificacion.java
@@ -0,0 +1,36 @@
+package cl.dsy1103.notificaciones.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "notificaciones")
+public class registro_notificacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_notificacion")
+    private Long idNotificacion;
+
+    @Column(name = "id_usuario")
+    private Long idUsuario;
+
+    @Column(name = "id_pedido")
+    private Long idPedido;
+
+    @Column(name = "tipo")
+    private String tipo;
+
+    @Column(name = "mensaje")
+    private String mensaje;
+
+    @Column(name = "fecha_envio")
+    private LocalDateTime fechaEnvio;
+}
