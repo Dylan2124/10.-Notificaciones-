ec9e69a52fc9cae19e9d57fa71050608719132ba
Matias Tiznado <m.tiznado@duocuc.cl>
2026-05-15 08:59:15 -0400

    Completado

--- src/main/java/cl/dsy1103/notificaciones/Application.java
+++ src/main/java/cl/dsy1103/notificaciones/Application.java
@@ -2,7 +2,9 @@ package cl.dsy1103.notificaciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class Application {
 
